59cccee34640c8b6289a0dd21c522cb820122da9
LucasIrvin <lucasgamerba@gmail.com>
2026-03-06 06:44:41 -0300

    Aula 1 - Comentarios e Docstrings

---
+++ aula1.py
@@ -0,0 +1,11 @@
+"""
+Isso é uma Docstring, esse código é um código lido pelo interpretador do Python mais podemos usar para comentar varias linhas!
+"""
+
+'''
+Usar para escrever suas notas
+'''
+
+# Comentário Python
+print(123) # Na frente
+# Abaixo
