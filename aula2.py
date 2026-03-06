58f8151ca5e606ef9fedf0d7c9ac51894f390fe0
LucasIrvin <lucasgamerba@gmail.com>
2026-03-06 07:02:46 -0300

    Aula 2 - Print e Argumentos

---
+++ aula2.py
@@ -0,0 +1,8 @@
+# A função print é utilizada para exibir as coisas na tela e ela recebe um negocio chamado argumento. Argumento é uma coisa que geralmente passamos para funções ou metodos. 
+
+# \r\n - \r = return. e \n = linefeed  -> CRLF
+# \n -> LF
+
+print(12, 34, sep="-", end="\n##")
+print(56, 78, sep="*", end="\n")
+print(9, 10, sep=".")
