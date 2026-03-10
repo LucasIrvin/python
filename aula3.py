6d764c670b917135d2afa4a128ed279c1b7a401c
LucasIrvin <lucasgamerba@gmail.com>
2026-03-10 06:40:28 -0300

    Aula 3 - Tipo String

---
+++ aula3.py
@@ -0,0 +1,29 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+
+print('Lucas Irvin')
+print(1, 'Lucas "Irvin"')
+
+
+# Aspas duplas
+
+print("Lucas Irvin")
+print(2, "Lucas 'Irvin'")
+
+
+# Escape
+
+print("Lucas \"Irvin\"")
+
+# r
+
+print(r"Lucas \"Irvin\"")
