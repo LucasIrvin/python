1cab99b777ad74bd5644949ff7f413ff6debce44
LucasIrvin <lucasgamerba@gmail.com>
2026-03-11 06:43:49 -0300

    Aula 6 - Coerção de tipos

---
+++ aula6.py
@@ -0,0 +1,12 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um timpo em outro
+# tipos imutaveis: Uma vez que crio esses tipos não consigo alterar eles no programa.
+# tipos primitivos: São tipos de dados mais usuais e básicos.
+# tipos imutáveis e primitivos:
+# str, int, float e bool
+
+print(int('1'), type(int('1')))
+print(type(float('1.2') + 1))
+print(bool('')) #String vazia = False e uma String preenchida é considerada True
+print(str(11) + 'b')
