f7e7b57dd18a01b153a97c051901b711a6378ea5
LucasIrvin <lucasgamerba@gmail.com>
2026-03-27 06:59:58 -0300

    Aula 28 - Correção

--- aula28.py
+++ aula28.py
@@ -17,7 +17,7 @@ Se nada for digitado em nome ou idade:
 nome = input('Digite seu nome: ')
 idade = input('Digite sua idade: ')
 
-if nome and idade != '':
+if nome and idade:
     print(f'Seu nome é {nome}')
     print(f'Seu nome invertido é {nome[::-1]}')
 
