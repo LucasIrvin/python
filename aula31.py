3ff758952c23cff6bf945c568b7fe3ae4dd5a05f
LucasIrvin <lucasgamerba@gmail.com>
2026-04-05 19:07:46 -0300

    Aula 31 - Flags, is, is not e None

--- aula31.py
+++ aula31.py
@@ -5,14 +5,16 @@ is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
 
-v1 = 'a'
-v2 = 'b'
-print(id(v1))
-print(id(v2))
+condicao = True
+passou_no_if = None
 
-# condicao = False
+if condicao:
+  print('Faça algo')
+  passou_no_if = True
+else:
+  print('Não faça algo')
 
-# if condicao:
-#   print('Faça algo')
-# else:
-#   print('Não faça algo')
+if passou_no_if is None:
+  print('Não passou no if')
+else:
+  print('Passou no if')
