ab0625cd5fa1c2de3f44a1ec100f953255df109f
LucasIrvin <lucasgamerba@gmail.com>
2026-04-07 06:06:54 -0300

    Aula 32 - Exercicio 5

---
+++ aula32.py
@@ -0,0 +1,45 @@
+
+# Exercicio 1
+"""
+try:
+    numero = input('Digite um número inteiro: ')
+    numero_int = int(numero)
+
+    if numero_int % 2 == 0:
+        print('Par')
+    else:
+        print('Impar')
+
+except:
+    print('O número digitado não é inteiro')
+
+"""
+
+# Exercicio 2
+"""
+horas = input('São quantas horas? ')
+horas_int = int(horas)
+
+if horas_int >= 0 and horas_int <= 11:
+    print('Bom dia')
+elif horas_int >= 12 and horas_int <= 17:
+    print('Boa tarde')
+elif horas_int >= 18 and horas_int <= 23:
+    print('Boa noite')
+
+"""
+
+# Exercicio 3
+
+"""
+nome = input('Digite seu primeiro nome: ')
+tamanho_nome = len(nome)
+
+if tamanho_nome <= 4:
+    print('Seu nome é curto')
+elif tamanho_nome == 5 or tamanho_nome == 6:
+    print('Seu nome é normal')
+else:
+    print('Seu nome é muito grande')
+
+"""
